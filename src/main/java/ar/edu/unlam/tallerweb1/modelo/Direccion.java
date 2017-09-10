f6eb1186074c07c93edc94135b3dc338988b3c79
MaxiAven <maxi.aven@yahoo.com.ar>
2017-09-10 17:42:33 -0300

    Test modificado
    Se genera test para buscar farmacias los dias martes y para farmacias de una calle

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -14,7 +15,7 @@ import javax.persistence.OneToMany;
 @Entity
 public class Direccion {
 	
-	@Id
+	@Id @GeneratedValue
 	private Long idDireccion;
 	private String calle;
 	private String numero;
--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestCriteria.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/TestCriteria.java
@@ -1,10 +1,14 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
+import java.util.List;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Barrio;
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
 import ar.edu.unlam.tallerweb1.modelo.Farmacia;
 
 public class TestCriteria extends SpringTest {
@@ -16,19 +20,67 @@ public class TestCriteria extends SpringTest {
 		
 		Farmacia f1= new Farmacia();
 		Farmacia f2 = new Farmacia();
+		Farmacia f3 = new Farmacia();
 		
 		f1.setDiadeturno("Lunes");
 		f1.setNombre("Vilela");
 		f1.setTelefono("44411234");
+		getSession().save(f1);
 		
 		f2.setDiadeturno("Martes");
 		f2.setNombre("Dr Ahorro");
 		f2.setTelefono("12345678");
-		
-		getSession().save(f1);
 		getSession().save(f2);
+		
+		f3.setDiadeturno("Martes");
+		f3.setNombre("Dr pirulo");
+		f3.setTelefono("12345678345");
+		getSession().save(f3);
+		
+		
+		List<Farmacia> farmacia;
+		
+		farmacia = getSession().createCriteria(Farmacia.class)
+				  .add(Restrictions.eq("diadeturno","Martes")).list();
+		
+		assertThat(farmacia).hasSize(2);
+		
+		
+		
 		
 		
 	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void BuscarFarmaciasDeUnaCalle() {
+		
+		Farmacia f1= new Farmacia();
+		Direccion dir1 = new Direccion();
+		Direccion dir2 = new Direccion();
+		//Barrio barrio = new Barrio();
+		
+		dir1.setCalle("San juan");
+		dir1.setNumero("3596");
+		getSession().save(dir1);
+		
+		f1.setDireccion(dir1);
+		
+		dir2.setCalle("San pedro");
+		dir2.setNumero("2336");
+		getSession().save(dir2);
+		
+		f1.setDireccion(dir2);
+		getSession().save(f1);
+		
+		List<Direccion> direccion;
+		
+		direccion = getSession().createCriteria(Direccion.class)
+				  .add(Restrictions.eq("calle","San Juan")).list();
+		
+		assertThat(direccion).hasSize(1);
+	}
+
 
 }
